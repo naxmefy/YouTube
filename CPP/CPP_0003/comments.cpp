d3396e23ffa6198034c5c84197095da21b0a5372
Mohammed Neundorf <mne@damncreative.de>
2014-09-07 04:05:29 +0200

    added cpp_0003 sources

---
+++ CPP/CPP_0003/comments.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+  // Dies ist ein einziliger Kommentar
+  /*
+    Dies ist ein mehrzeiliger Kommentar.
+    Er geht über mehrere Zeilen.
+    Das ist ja mal total witzig.
+  */
+
+  int x = 3; // Hier wird eine Integer Variable X mit 3 definiert.
+  int y = 4; // Hier wird eine Integer Variable Y mit 4 definiert.
+
+  int z = x + y; // Here we define an interger variable with the sum of x and y.
+
+  // 1 Woche für Entwicklung =>
+  // 1 Woche für Dokumantion einplanen
+
+  return 0;
+}
